72a1658e74783ae064df2a4a4f4d78dd68b60b8f
Vladislav Kim <vladkim165@gmail.com>
2021-09-20 16:45:53 +0300

    feature/pizzasSortJS

--- script.js
+++ script.js
@@ -1,29 +1,4 @@
-const template = document.querySelector('.card-template')
-const newCard = template.content.cloneNode(true)
-const newCard1 = template.content.cloneNode(true)
-const newCard2 = template.content.cloneNode(true)
-const newCard3 = template.content.cloneNode(true)
-const newCard4 = template.content.cloneNode(true)
-const newCard5 = template.content.cloneNode(true)
-const newCard6 = template.content.cloneNode(true)
-const newCard7 = template.content.cloneNode(true)
-const newCard8 = template.content.cloneNode(true)
-const newCard9 = template.content.cloneNode(true)
-const newCard11 = template.content.cloneNode(true)
-const newCard12 = template.content.cloneNode(true)
-const list = document.querySelector('.elements-list')
-list.prepend(newCard)
-list.prepend(newCard1)
-list.prepend(newCard2)
-list.prepend(newCard3)
-list.prepend(newCard4)
-list.prepend(newCard5)
-list.prepend(newCard6)
-list.prepend(newCard7)
-list.prepend(newCard8)
-list.prepend(newCard9)
-list.prepend(newCard11)
-list.prepend(newCard12)
+// обьект с данными слайдера
 
 const promoWindows = [
   {
@@ -52,6 +27,9 @@ const promoTitle = mainPromoCard.querySelector('.promo-card__title')
 const promoText = mainPromoCard.querySelector('.section-text')
 const progressButtons = document.querySelectorAll('.promo__progress-button')
 
+
+// функция изменения текста и картинки слайдера
+
 const setEventsOnPromoButton = (button, obj, value) => {
   button.addEventListener('click', () => {
     promoImage.src = obj[value].image;
@@ -68,8 +46,6 @@ setEventsOnPromoButton(firstPromoButton, promoWindows, 0)
 setEventsOnPromoButton(secondPromoButton, promoWindows, 1)
 setEventsOnPromoButton(thirdPromoButton, promoWindows, 2)
 
-
-
 const button = document.querySelector('.header__menu-button')
 const menu = document.querySelector('.menu')
 const closeButton = menu.querySelector('.menu__close-button')
@@ -80,4 +56,201 @@ closeButton.addEventListener('click', () => {
 
 button.addEventListener('click', () => {
   menu.classList.toggle('menu_active')
+})
+
+
+// объект с данными пицц
+const pizzas = [
+  {
+    name: 'Итальянская',
+    description: 'Томат, шампиньон, сыр, оливки, чили, соус, тесто, базилик',
+    priceS: '499',
+    priceM: '699',
+    priceL: '899',
+    image: 'images/size_m.png',
+    type: 'spicy',
+    typeimg: 'images/pizzas-spicy.svg'
+  },
+  {
+    name: 'Маргарита',
+    description: 'Тесто со шпинатом, молодой сыр и колбаски, много колбасок',
+    priceS: '499',
+    priceM: '599',
+    priceL: '699',
+    image: 'images/size_m.png',
+    type: 'cheesy',
+    typeimg: 'images/pizzas-cheesy.svg'
+  },
+  {
+    name: 'Барбекю',
+    description: 'Цыплёнок (маленький кура), оливки, моцарелла, соус барбекю',
+    priceS: '499',
+    priceM: '699',
+    priceL: '899',
+    image: 'images/size_m.png',
+    type: 'meat',
+    typeimg: 'images/pizzas-meat.svg'
+  },
+  {
+    name: 'Вегетарианская',
+    description: 'Томат, шампиньон, сыр, оливки, чили, соус, тесто, базилик',
+    priceS: '349',
+    priceM: '549',
+    priceL: '749',
+    image: 'images/size_m.png',
+    type: 'vegan',
+    typeimg: 'images/pizzas-vegan.svg'
+  },
+  {
+    name: 'Мясная',
+    description: 'Томат, шампиньон, сыр, оливки, чили, соус, тесто, базилик',
+    priceS: '699',
+    priceM: '899',
+    priceL: '1099',
+    image: 'images/size_m.png',
+    type: 'meat',
+    typeimg: 'images/pizzas-meat.svg'
+  },
+  {
+    name: 'Овощная',
+    description: 'Тесто со шпинатом, молодой сыр и колбаски, много колбасок',
+    priceS: '399',
+    priceM: '499',
+    priceL: '599',
+    image: 'images/size_m.png',
+    type: 'vegan',
+    typeimg: 'images/pizzas-vegan.svg'
+  },
+  {
+    name: 'Римская',
+    description: 'Цыплёнок (маленький кура), оливки, моцарелла, соус барбекю',
+    priceS: '599',
+    priceM: '699',
+    priceL: '799',
+    image: 'images/size_m.png',
+    type: 'meat',
+    typeimg: 'images/pizzas-meat.svg'
+  },
+  {
+    name: 'С грибами',
+    description: 'Томат, шампиньон, сыр, оливки, чили, соус, тесто, базилик',
+    priceS: '799',
+    priceM: '899',
+    priceL: '999',
+    image: 'images/size_m.png',
+    type: 'spicy',
+    typeimg: 'images/pizzas-spicy.svg'
+  },
+  {
+    name: 'Сырная',
+    description: 'Томат, шампиньон, сыр, оливки, чили, соус, тесто, базилик',
+    priceS: '599',
+    priceM: '699',
+    priceL: '799',
+    image: 'images/size_m.png',
+    type: 'cheesy',
+    typeimg: 'images/pizzas-cheesy.svg'
+  },
+  {
+    name: 'Четыре сыра',
+    description: 'Тесто со шпинатом, молодой сыр и колбаски, много колбасок',
+    priceS: '599',
+    priceM: '659',
+    priceL: '899',
+    image: 'images/size_m.png',
+    type: 'cheesy',
+    typeimg: 'images/pizzas-cheesy.svg'
+  },
+  {
+    name: 'Пепперони Фреш с томатами',
+    description: 'Цыплёнок (маленький кура), оливки, моцарелла, соус барбекю',
+    priceS: '499',
+    priceM: '699',
+    priceL: '899',
+    image: 'images/size_m.png',
+    type: 'spicy',
+    typeimg: 'images/pizzas-spicy.svg'
+  },
+  {
+    name: 'Ветчина и сыр',
+    description: 'Томат, шампиньон, сыр, оливки, чили, соус, тесто, базилик',
+    priceS: '599',
+    priceM: '699',
+    priceL: '799',
+    image: 'images/size_m.png',
+    type: 'meat',
+    typeimg: 'images/pizzas-meat.svg'
+  }
+]
+
+const list = document.querySelector('.elements-list')
+const template = document.querySelector('.card-template')
+
+// Добавляем пиццы в контейнер
+
+const setTemplate = (data) => {
+  const newCard = template.content.cloneNode(true)
+  const cardName = newCard.querySelector('.element__title')
+  const cardDescription = newCard.querySelector('.element__description')
+  const cardPrice = newCard.querySelector('.element__price-number')
+  const cardType = newCard.querySelector('.element__type-image')
+
+  cardName.textContent = data.name;
+  cardDescription.textContent = data.description;
+  cardPrice.textContent = data.priceM;
+  cardType.src = data.typeimg;
+  list.prepend(newCard)
+}
+
+pizzas.forEach(item => {
+  setTemplate(item)
+})
+
+const all = document.getElementById('all')
+const spicy = document.getElementById('spicy')
+const meat = document.getElementById('meat')
+const cheesy = document.getElementById('cheesy')
+const vegan = document.getElementById('vegan')
+
+all.addEventListener('click', () => {
+  list.innerHTML = '';
+  pizzas.forEach(item => {
+    setTemplate(item)
+  })
+})
+
+spicy.addEventListener('click', () => {
+  list.innerHTML = '';
+  pizzas.forEach(item => {
+    if (item.type === 'spicy') {
+      setTemplate(item)
+    }
+  })
+})
+
+meat.addEventListener('click', () => {
+  list.innerHTML = '';
+  pizzas.forEach(item => {
+    if (item.type === 'meat') {
+      setTemplate(item)
+    }
+  })
+})
+
+cheesy.addEventListener('click', () => {
+  list.innerHTML = '';
+  pizzas.forEach(item => {
+    if (item.type === 'cheesy') {
+      setTemplate(item)
+    }
+  })
+})
+
+vegan.addEventListener('click', () => {
+  list.innerHTML = '';
+  pizzas.forEach(item => {
+    if (item.type === 'vegan') {
+      setTemplate(item)
+    }
+  })
 })
